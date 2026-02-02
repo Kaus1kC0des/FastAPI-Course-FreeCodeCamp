683dc72074d4e7ea660d10339fbac072b61ea698
Kausik D <kausikdevanathan@gmail.com>
2026-02-02 17:53:46 +0530

    Completed Data Ingestion for Dummy Users

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,43 +1,42 @@
+from app.services.auth_service import get_current_user
 from app.schemas.user import UserCreate, UserResponse
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, insert
-from fastapi import APIRouter, status, HTTPException
-from app.models import Users
+from fastapi import APIRouter, status
 from app.dependencies import get_db_async
-from app.services.auth_service import hash
-from uuid import UUID
+from app.services import user_service
 from typing import Annotated
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(
+async def create_user_route(
     user: UserCreate, db: Annotated[AsyncSession, Depends(get_db_async)]
 ):
     try:
-        data = user.model_dump(exclude={"confirm_password"})
-        data["password"] = hash(data["password"])
-        stmt = insert(Users).values(data).returning(Users.id)
-        result = await db.scalar(stmt)
-        await db.commit()
-        return {"result": "User Created Successfully!", "id": result}
+        result = await user_service.create_user(user, db)
+        return result
     except Exception as e:
-        await db.rollback()
         raise e
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=UserResponse)
-async def get_user(id: UUID, db: Annotated[AsyncSession, Depends(get_db_async)]):
+async def get_user_route(id: int, db: Annotated[AsyncSession, Depends(get_db_async)]):
     try:
-        stmt = select(Users).where(Users.id == id)
-        result = await db.scalar(stmt)
-        if not result:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"User with id = {id} not found",
-            )
+        result = await user_service.get_user(id, db)
+        return result
+    except Exception as e:
+        raise e
+
+
+@router.delete("/delete")
+async def delete_user(
+    user_id: Annotated[int, Depends(get_current_user)],
+    db: Annotated[AsyncSession, Depends(get_db_async)],
+):
+    try:
+        result = await user_service.delete_user(user_id, db)
         return result
     except Exception as e:
         raise e
